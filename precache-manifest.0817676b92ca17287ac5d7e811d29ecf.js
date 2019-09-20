2282a66685f7fae57d6df22386bcae09ee465f6a
Usmaan Ali <usmaan.ali@radiuspaymentsolutions.com>
2019-09-20 22:50:07 +0100

    Updates

---
+++ precache-manifest.0817676b92ca17287ac5d7e811d29ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6baf664f804132859dc4be16cbe98af2",
+    "url": "/scunthorpe-app/index.html"
+  },
+  {
+    "revision": "b773dbde09338bce8ce3",
+    "url": "/scunthorpe-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "36212995a8b017648ed4",
+    "url": "/scunthorpe-app/static/js/2.cd930777.chunk.js"
+  },
+  {
+    "revision": "b773dbde09338bce8ce3",
+    "url": "/scunthorpe-app/static/js/main.67b22be0.chunk.js"
+  },
+  {
+    "revision": "816755ad310c9a2d9a66",
+    "url": "/scunthorpe-app/static/js/runtime-main.19876d3a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/scunthorpe-app/static/media/logo.25bf045c.svg"
+  }
+]);
